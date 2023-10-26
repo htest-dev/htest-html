e07fd9888f7220fcff0f6d38a8935d4f921d014b
Lea Verou <lea@verou.me>
2023-10-26 19:31:20 -0400

    Refactor to prepare for JS-first version
    - Moved HTML-specific stuff to src/html/
    - Separated out comparator logic from DOM stuff

---
+++ harness.js
@@ -0,0 +1,91 @@
+import { create, $$ } from "./util.js";
+
+/**
+ * Code for test homepage
+ */
+
+function runSelected (names) {
+	iframes.innerHTML = "";
+
+	let tests = names.map(name => {
+		return create("iframe", {
+			inside: iframes,
+			src: name,
+			events: {
+				load: evt => {
+					updateTotals();
+
+					evt.target.contentWindow.document.addEventListener("testresultsupdate", evt => {
+						updateTotals();
+					});
+				}
+			}
+		});
+	});
+
+	function updateTotals() {
+		let totals = {pass: 0, fail: 0};
+
+		tests.forEach(iframe => {
+			let doc = iframe.contentDocument;
+
+			if (doc.readyState !== "complete" || !doc.querySelector("body > nav")) {
+				return;
+			}
+
+			totals.pass += +doc.querySelector("body > nav .count-pass .count").textContent;
+			totals.fail += +doc.querySelector("body > nav .count-fail .count").textContent;
+		});
+
+		let totalsEl = document.querySelector("h1 + .totals") || create({className: "totals", after: document.querySelector("h1")});
+
+		totalsEl.innerHTML = `<strong>${totals.pass}</strong> passing, <strong>${totals.fail}</strong> failing of ${totals.pass + totals.fail} total`
+	}
+}
+
+// tests is the <ul id="tests"> that contains the list of tests
+for (let a of tests.querySelectorAll("li > a")) {
+	let li = a.parentElement;
+	a.insertAdjacentHTML("beforebegin", `<input type="checkbox" property="toRun" checked>`);
+	a.insertAdjacentHTML("afterend", `<a href="${ a.getAttribute("href") }" class="new-tab" title="Open in new Tab" target="_blank">↗️</a>`);
+
+	a.target = "test";
+	li.addEventListener("click", evt => {
+		if (evt.target.matches("a")) {
+			for (let li of tests.querySelectorAll("li")) {
+				li.classList.remove("selected");
+			}
+			li.classList.add("selected");
+			iframes.textContent = "";
+		}
+		else if (evt.target.matches("input[type=checkbox]")) {
+			let checked = $$("input[type=checkbox]:not(#select_all)", tests).map(checkbox => checkbox.checked);
+			let includesTrue = checked.includes(true);
+			let includesFalse = checked.includes(false);
+			select_all.indeterminate = includesTrue && includesFalse;
+			select_all.checked = includesTrue;
+		}
+	});
+}
+
+tests.insertAdjacentHTML("afterbegin", `<li>
+<label>
+	<input type="checkbox" id="select_all" checked>
+	Select/unselect all
+</label>
+</li>`)
+tests.insertAdjacentHTML("afterend", `<button id="run_button">Run selected</button>`);
+
+select_all.addEventListener("click", evt => {
+	let checked = evt.target.checked;
+	for (let checkbox of $$("input[type=checkbox]:not(#select_all)", tests)) {
+		checkbox.checked = checked;
+	}
+});
+
+run_button.addEventListener("click", evt => {
+	runSelected($$("li > :checked + a", tests).map(a => a.getAttribute("href")));
+});
+
+document.body.insertAdjacentHTML("beforeend", `<div id="iframes"></div>
+<iframe name="test" src="about:blank"></iframe>`);
---
+++ reftest.js
@@ -0,0 +1,297 @@
+import { doClick, create, $$, bind, } from "./util.js";
+import hooks from "../hooks.js";
+import * as compare from "./compare.js";
+
+export default class RefTest {
+	constructor (table) {
+		this.table = table;
+		this.columns = +this.table.getAttribute("data-columns") || Math.max.apply(Math, [...this.table.rows].map(row => row.cells.length));
+		this.init();
+	}
+
+	async init () {
+		this.compare = await RefTest.getTest(this.table.getAttribute("data-test"));
+		this.setup();
+		this.startup = performance.now();
+		this.test();
+	}
+
+	setup () {
+		// Remove any <script> elements to prevent them messing up the contents. They've already been processed anyway.
+		// Keep them in an attribute though, as they may be useful to display
+		// for (let script of $$("script", this.table)) {
+		// 	if (!script.type || script.type === "text/javascript") {
+		// 		script.remove();
+		// 	}
+		// }
+
+		if (this.table.rows.length === 0) {
+			console.warn("Empty reftest:", this.table);
+			return;
+		}
+
+		// Add table header if not present
+		var cells = [...this.table.rows[0].cells];
+
+		if (!this.table.querySelector("thead") && this.columns > 1) {
+			var header = [...Array(Math.max(0, this.columns - 2)).fill("Test"), "Actual", "Expected"].slice(-this.columns);
+
+			create("thead", {
+				contents: [
+					{
+						tag: "tr",
+						contents: header.map(text => {
+							return {tag: "th", textContent: text};
+						})
+					}
+				],
+				start: this.table
+			});
+		}
+
+		var test = x => {
+			requestAnimationFrame(() => this.test());
+		};
+
+		this.observer = new MutationObserver(test);
+		this.observe();
+
+		bind(this.table, "input change click mv-change", test);
+
+		this.table.closest("[mv-app]")?.addEventListener("mv-load", test);
+
+		$$("[data-click]", this.table)
+			.concat(this.table.matches("[data-click]")? [this.table] : [])
+			.forEach(target => {
+				target.getAttribute("data-click").trim().split(/\s*,\s*/).forEach(doClick);
+			});
+	}
+
+	observe () {
+		this.observerRunning = true;
+
+		// TODO move this somewhere else, it's Mavo specific
+		this.observer.observe(this.table, {
+			subtree: true,
+			childList: true,
+			attributes: true,
+			characterData: true,
+			attributeFilter: ["mv-mode"]
+		});
+	}
+
+	unobserve () {
+		this.observer.disconnect();
+		this.observerRunning = false;
+	}
+
+	// Run code past observer
+	sneak (callback) {
+		this.unobserve();
+		var ret = callback.call(this);
+		this.observe();
+		return ret;
+	}
+
+	test () {
+		hooks.run("reftest-test", this);
+
+		for (let tr of this.table.rows) {
+			if (!this.table.tHead || tr !== this.table.tHead.rows[0]) {
+				this.testRow(tr);
+			}
+		}
+	}
+
+	async testRow (tr) {
+		let env = {context: this, tr, cells: [...tr.cells]};
+		hooks.run("reftest-testrow", env);
+
+		if (!env.tr.compare) {
+			env.tr.compare = await RefTest.getTest(env.tr.getAttribute("data-test"), this.compare);
+		}
+
+		let resultCell = env.tr.cells[env.tr.cells.length - 1];
+
+		if (env.cells.length) {
+			if (this.columns == 3) {
+				// Test, actual, expected
+				if (env.cells.length == 1) {
+					// expected is the same as test
+					resultCell = create("td", {after: env.cells[0]});
+					env.cells.push(resultCell);
+				}
+
+				if (env.cells.length == 2) {
+					// missing actual
+					resultCell = create("td", {after: env.cells[0]});
+					env.cells.splice(1, 0, resultCell);
+				}
+
+				if (!env.cells[2].textContent) {
+					env.cells[2].textContent = env.cells[0].textContent;
+				}
+			}
+			else if (this.columns == 2 && !env.cells[0].innerHTML) {
+				// Empty cell, takes the test from above
+				let previous = env.tr;
+				while (previous = previous.previousElementSibling) {
+					if (previous.cells[0].innerHTML) {
+						env.cells[0] = previous.cells[0];
+						break;
+					}
+				}
+			}
+
+			try {
+				var ret = this.sneak(() => tr.compare(...env.cells));
+				resultCell.onclick = null;
+			}
+			catch (e) {
+				ret = e;
+				var error = true;
+				resultCell.textContent = e + "";
+				resultCell.onclick = evt => console.error(e);
+			}
+
+			var error = ret instanceof Error;
+
+			var previousClass = tr.classList.contains("pass")? "pass" : "fail";
+			tr.classList.remove("pass", "fail");
+			let pass = ret;
+			if (error) {
+				pass = tr.hasAttribute("data-error");
+			}
+
+			var className = pass? "pass" : "fail";
+			tr.classList.add(className);
+
+			if (className == "pass" && className != previousClass && !tr.classList.contains("interactive")) {
+				// Display how long it took
+				var time = performance.now() - this.startup;
+				var unit = "ms";
+
+				time = +time.toFixed(2);
+
+				if (time > 100) {
+					time = Math.round(time);
+				}
+
+				if (time > 1000) {
+					time /= 1000;
+					unit = "s";
+				}
+
+				tr.setAttribute("data-time", time + unit);
+			}
+
+			RefTest.updateResults();
+		}
+	}
+
+	static hooks = hooks
+
+
+	// Retrieve the comparator function based on a data-test string
+	static async getTest (test, fallback) {
+		if (test) {
+			if (test in this.compare) {
+				return this.compare[test];
+			}
+			else {
+				if (test in globalThis) {
+					return globalThis[test];
+				}
+				else {
+					// return new Function("td", "ref", test);
+					// Try again in a bit
+					await new Promise(resolve => window.addEventListener("load", resolve, {once: true}));
+					return globalThis[test];
+				}
+			}
+		}
+
+		return fallback || RefTest.compare.contents;
+	}
+
+	// Default comparison functions
+	static compare = compare
+
+	// Prettify code for presentation
+	static presentCode (code) {
+		// Remove blank line in the beginning and end
+		code = code.replace(/^\s*\n|\n\s*$/g, "");
+
+		// Remove extra indentation
+		var indent = (code.match(/^\s*/) || [""])[0];
+		code = code.replace(RegExp("^" + indent, "gm"), "");
+
+		code = code.replace(/document.write/g, "print");
+
+		return code;
+	}
+
+	static updateResults () {
+		this.results = {
+			pass: $$("table.reftest:not(.ignore) tr.pass:not(.ignore)"),
+			fail: $$("table.reftest:not(.ignore) tr.fail:not(.ignore)"),
+			current: {
+				pass: -1,
+				fail: -1
+			}
+			// interactive: $$("table.reftest tr.interactive")
+		};
+
+		var detail = {
+			pass: this.results.pass.length,
+			fail: this.results.fail.length
+		};
+
+		let nav = document.querySelector("body > nav");
+
+		nav.querySelector(".count-pass .count").textContent = detail.pass;
+		nav.querySelector(".count-fail .count").textContent = detail.fail;
+
+		nav.style.setProperty("--pass", detail.pass);
+		nav.style.setProperty("--fail", detail.fail);
+
+		// $(".count-interactive", RefTest.nav).textContent = RefTest.results.interactive.length;
+
+		document.dispatchEvent(new CustomEvent("testresultsupdate", {detail}));
+	}
+
+	// Navigate tests
+	static #navigateTests (type = "fail", offset) {
+		let elements = this.results[type];
+		let i = this.results.current[type] + offset;
+
+		if (!elements.length) {
+			return;
+		}
+
+		if (i >= elements.length) {
+			i = 0;
+		}
+		else if (i < 0) {
+			i = elements.length - 1;
+		}
+
+		if (elements.length > 1) {
+			let countElement = RefTest.nav.querySelector(".count-" + type);
+			countElement.querySelector(".nav").hidden = false;
+			countElement.querySelector(".current").textContent = i + 1;
+		}
+
+		elements[i].scrollIntoView({behavior: "smooth"});
+
+		this.results.current[type] = i;
+	}
+
+	static next (type = "fail") {
+		this.#navigateTests(type, 1);
+	}
+
+	static previous (type = "fail") {
+		this.#navigateTests(type, -1);
+	}
+}
